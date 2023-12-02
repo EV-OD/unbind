1ce0640205c4231b073ac4b7685da9385a49035b
EV-OD <evod599@gmail.com>
2023-12-02 20:55:16 +0545

    add subscription

--- content.js
+++ content.js
@@ -27,6 +27,23 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 function updateUnBind() {
   removeUIElements();
+  redirectSubscription();
+}
+
+function redirectSubscription() {
+  if (feature.subscription) {
+    var currentURL = window.location.href;
+
+    if (!/\/subscriptions$/.test(currentURL)) {
+      var subscriptionURL =
+        window.location.protocol +
+        "//" +
+        window.location.host +
+        "/subscriptions";
+
+      window.location.replace(subscriptionURL);
+    }
+  }
 }
 
 function removeUIElements() {

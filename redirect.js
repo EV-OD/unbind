a8373d76c0843421a9f8ce552e6a3938e55d6bf7
Atul0002 <1atultiwari062@gmail.com>
2023-12-02 21:25:28 +0545

    remove redirect

--- redirect.js
+++
@@ -1,9 +0,0 @@
-var currentURL = window.location.href;
-
-if (!/\/subscriptions$/.test(currentURL)) {
-    var subscriptionURL = window.location.protocol + "//"
-                            + window.location.host
-                            + "/subscriptions";
-
-    window.location.replace(subscriptionURL);
-}
